f10b2df67220718b7444f6d3fdfb9a95db7d58bd
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-01-26 21:15:09 +0200

    feat: add navigation and footer

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,26 @@
+import React from "react"
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider,} from "react-router-dom";
+import Navigation from "./components/navigation";
+import Analytics from "./pages/analytics";
+
+let router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Navigation/>}>
+            <Route index element={<h2>Index</h2>} />
+            <Route path="game1" element={<h2>Game1</h2>} />
+            <Route path="game2" element={<h2>Game2</h2>} />
+            <Route path="game3" element={<h2>Game3</h2>} />
+            <Route path="analytics" element={<Analytics/>} />
+        </Route>
+    )
+);
+
 export const App = () => {
     return (
         <>
-            <h1>React TypeScript Webpack Starter Template</h1>
+            <React.StrictMode>
+                <RouterProvider router={router} />
+            </React.StrictMode>
         </>
     )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+
+    return (
+        <div className='footer'>
+            <a href='https://github.com/adwasar'>@adwasar</a>
+            <a href='https://github.com/pnmrvvtl'>@pnmrvvtl</a>
+            <a href='https://github.com/zhannach'>@zhannach</a>
+        </div>
+    );
+}
