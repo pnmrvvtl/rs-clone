4a18cdd6342b379067e75a022316c31a4c8f22fb
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-02-01 09:51:28 +0200

    feat: add testing images

--- src/components/meal/meal.tsx
+++ src/components/meal/meal.tsx
@@ -1,9 +1,12 @@
 import styles from './meal.module.scss';
+import photo from "../../assets/hf.jpg";
 
 export default function Meal() {
     return (
         <div className={styles.container}>
             Meal
+            <div className={styles['image-container']}></div>
+            <img src={photo} width={100} height={100}/>
         </div>
     )
 }
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,9 +1,12 @@
 import styles from './main-page.module.scss';
+import photo from '../../assets/hf.jpg';
 
 export default function () {
     return (
         <div className={styles.container}>
             Main-Page
+            <div className={styles['image-container']}></div>
+            <img src={photo} width={100} height={100}/>
         </div>
     )
 }
