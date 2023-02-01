1a5f9ec484b36f5b91cd5e08a09997ef1dc0a2f1
Zhanna <zhanna01992@gmail.com>
2023-02-02 00:39:46 +0200

    feat: add some main page layout

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,12 +1,14 @@
-import styles from './main-page.module.scss';
 import photo from '../../assets/hf.jpg';
+import styles from './main-page.module.scss'
+import AboutInfo from '../../components/main/AboutInfo'
+import Benefits from '../../components/main/Benefits'
 
-export default function () {
+export default function MainPage () {
     return (
-        <div className={styles.container}>
-            Main-Page
-            <div className={styles['image-container']}></div>
-            <img src={photo} width={100} height={100}/>
-        </div>
+       <div>
+       <AboutInfo/>
+       <Benefits/>
+       {/* <Slides/> */}
+       </div>
     )
 }
