48039ee8f2bd14761f36f15d46d6a61ef772a731
Zhanna Chaikovska <87633082+zhannach@users.noreply.github.com>
2023-02-02 21:34:03 +0200

    feat: add main page layout (#2)
    * feat: add some main page layout
    
    * feat: add instruction section
    
    * feat: add slider component
    
    * feat: complete main-page layout

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,12 +1,15 @@
-import styles from './main-page.module.scss';
-import photo from '../../assets/hf.jpg';
+import AboutInfo from '../../components/main/AboutInfo'
+import Benefits from '../../components/main/Benefits'
+import Instruction from '../../components/main/Instruction'
+import Slider from '../../components/main/Slider';
 
-export default function () {
+export default function MainPage () {
     return (
-        <div className={styles.container}>
-            Main-Page
-            <div className={styles['image-container']}></div>
-            <img src={photo} width={100} height={100}/>
-        </div>
+       <div>
+       <AboutInfo/>
+       <Benefits/>
+       <Instruction/>
+       <Slider/>
+       </div>
     )
 }
