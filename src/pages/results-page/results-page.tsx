a354486b317a3489468e4179a411fab0f550b982
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-02-03 18:02:19 +0200

    feat: add user data context

--- src/pages/results-page/results-page.tsx
+++ src/pages/results-page/results-page.tsx
@@ -1,7 +1,12 @@
+import {useContext} from "react";
+import {UserContext} from "../../context/user-context";
+
 export default function ResultsPage() {
+    const {userData} = useContext(UserContext);
+
     return (
         <div>
-            Results Page
+            {JSON.stringify(userData)}
         </div>
     )
 }
