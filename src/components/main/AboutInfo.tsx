cefeba819752203840ec8fa4990bedc58b823a47
Zhanna <zhanna01992@gmail.com>
2023-02-03 13:52:10 +0200

    feat: add adaptive and responsive design

--- src/components/main/AboutInfo.tsx
+++ src/components/main/AboutInfo.tsx
@@ -8,7 +8,6 @@ export default function AboutInfo() {
       <h5 className={styles["about-subtitle"]}>
         Custom meal plans, delicious recipes and grocery lists <br/> to help you stay fit and feel better.
       </h5>
-      <div className={styles["about-img"]}></div>
       <button className={styles["about-btn"]}>Start free trial</button>
     </section>
   )
