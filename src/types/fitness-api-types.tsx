d4ec1497779bd66972251094569184c1a1be3aba
Zhanna <zhanna01992@gmail.com>
2023-02-06 11:25:36 +0200

    feat: add fitness-api

---
+++ src/types/fitness-api-types.tsx
@@ -0,0 +1,81 @@
+
+export type FitnessApiResponse<T> = {
+    data: T
+    status: number
+    result: string
+}
+export interface BMIParams {
+    age: number
+    weight: number
+    height: number
+}
+
+export interface DailyCaloryParams extends BMIParams {
+    gender: string
+}
+
+export interface MacrosParams extends DailyCaloryParams {
+    activitylevel: string
+    goal: string
+}
+
+export type Calory = {
+    bmr: number,
+    goals: {
+        maintaine: number,
+        mildLoss: {
+            weigth: string,
+            calory: number
+        },
+        loss: {
+            weigth: string,
+            calory: number
+        },
+        extremeLoss: {
+            weigth: string,
+            calory: number
+        },
+        mildGain: {
+            weigth: string,
+            calory: number
+        },
+        gain: {
+            weigth: string,
+            calory: number
+        },
+        extremeGain: {
+            weigth: string,
+            calory: number
+        }
+    }
+}
+
+export type BMI = {
+    bmi: number
+    health: string
+    healthy_bmi_range: string
+}
+
+export type Macros = {
+    calorie: number,
+    balanced: {
+        protein: number
+        fat: number
+        carbs: number
+    },
+    lowfat: {
+        protein: number
+        fat: number
+        carbs: number
+    },
+    lowcarbs: {
+        protein: number
+        fat: number
+        carbs: number
+    },
+    highprotein: {
+        protein: number
+        fat: number
+        carbs: number
+    }
+}
