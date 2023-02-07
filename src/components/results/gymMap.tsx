fdd703fc996cf53144cc9292dc517d9ad4fa6de2
Zhanna Chaikovska <87633082+zhannach@users.noreply.github.com>
2023-02-07 21:06:10 +0200

    feat: add results-page layout (#12)

---
+++ src/components/results/gymMap.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '../../pages/results-page/results-page.module.scss'
+
+export default function gymMap() {
+  return (
+    <div className={styles["gym-map"]}>gymMap</div>
+  )
+}
--- src/pages/results-page/results-page.tsx
+++ src/pages/results-page/results-page.tsx
@@ -1,8 +1,14 @@
-import React, {useContext, useEffect, useLayoutEffect, useState} from 'react';
-import {UserContext} from "../../context/user-context";
+import React, { useContext, useEffect, useLayoutEffect, useState } from 'react';
+import BMI from '../../components/results/bmi';
+import styles from '../../pages/results-page/results-page.module.scss'
+import { UserContext } from "../../context/user-context";
+import Article from '../../components/results/article';
+import BmiChart from '../../components/results/bmi-chart';
+import MealPlan from '../../components/results/meal-plan';
+import GymMap from '../../components/results/gymMap';
 
 export default function ResultsPage() {
-    const {userData} = useContext(UserContext);
+    const { userData } = useContext(UserContext);
 
     useEffect(() => {
         //api calls here
@@ -10,10 +16,19 @@ export default function ResultsPage() {
     }, [/*userData*/]);
 
     return (
-        <div> Result Page
-            {/*<section style={{width: '200px'}}>*/}
-            {/*    {JSON.stringify(userData)}*/}
-            {/*</section>*/}
-        </div>
+        <main className={styles.content}>
+            <Article/>
+            <h1 className={styles["result-title"]}>Your results</h1>
+            <section className={styles.bmi}>
+                <BMI />
+                <BmiChart />
+                {/* <section style={{width: '200px'}}>
+               {JSON.stringify(userData)}
+            </section> */}
+            </section>
+            <MealPlan />
+            <GymMap/>
+        </main>
+
     )
 }
