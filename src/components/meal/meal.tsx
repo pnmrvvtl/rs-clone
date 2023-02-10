d64d1451c8f1c3f084898364b92d59718914a2cc
Vladislav <104913853+Adwasar@users.noreply.github.com>
2023-02-10 19:20:06 +0200

    feat: content generation from api on meal page
    * style: meal-img posicion
    
    * feat: set summary description
    
    * render ingredients
    
    * feat: rendering ingredients
    
    * fix: genereate ingridients
    
    ---------
    
    Co-authored-by: Adwasar <user@Users-MacBook-Pro.local>

--- src/components/meal/meal.tsx
+++ src/components/meal/meal.tsx
@@ -1,39 +1,51 @@
 import styles from './meal.module.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faClock} from '@fortawesome/free-solid-svg-icons';
-import {useParams} from "react-router";
-import {useContext, useEffect} from "react";
-import {UserContext} from "../../context/user-context";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { useParams } from "react-router";
+import { useContext, useEffect } from "react";
+import { UserContext } from "../../context/user-context";
 import ErrorPage from "../../pages/error-page/error-page";
+// import { CommentBankSharp } from '@mui/icons-material';
+import { ExtendedIngredient, Step } from "../../types/meals-api-types";
 
 export default function Meal() {
-    let {id} = useParams();
+    const { id } = useParams();
 
     useEffect(() => {
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0);
     }, [id]);
 
-    const {mealsByParametersResponse} = useContext(UserContext);
+    const { mealsByParametersResponse } = useContext(UserContext);
     const meal = mealsByParametersResponse.results.filter((el) => el.id === +id!)[0];
+    console.log(meal);
 
     if (!meal) {
-        return <ErrorPage/>
+        return <ErrorPage />
     }
 
     return (
         <div className={styles.container}>
-            <div className={styles['background-img']} style={{backgroundImage: `url('${meal.image}')`}}>
-                <div className={styles['content-wrapper']}>
-                    <DescriptionBox title={meal.title} duration = {meal.readyInMinutes}/>
+            <div className={styles['description-wrapper']}>
+                <div className={styles['description__content-wrapper']}>
+                    <DescriptionBox title={meal.title} duration={meal.readyInMinutes} summary={meal.summary} />
                 </div>
+                <img
+                    className={styles['description__meal-img']}
+                    src={`${meal.image}`}>
+                </img>
             </div>
+
             <div className={styles['content-wrapper']}>
                 <div className={styles.content}>
-                    <Ingredients/>
+                    <div className={styles['ingredients']}>
+                        <h3>Ingredients</h3>
+                        <IngredientsList extendedIngredients={meal.extendedIngredients} />
+                    </div>
+
                     <div className={styles['main-content']}>
-                        <RecipeSection/>
-                        <Tip/>
+                        <Instructions steps={meal.analyzedInstructions[0].steps}/>
+                        <Tip />
                     </div>
                 </div>
             </div>
@@ -42,42 +54,50 @@ export default function Meal() {
     )
 }
 
-const DescriptionBox = (props: { title: string , duration: number}) => {
+
+const DescriptionBox = (props: { title: string, duration: number, summary: string }) => {
     return (
-        <div className={styles['description-box-wrapper']}>
-            <div className={styles['description-box']}>
-                <h1 className={styles['description-box__title']}>{props.title}</h1>
-                <Text/>
-                <aside className={styles['description-box__time-info']}>
-                    <span className={styles['description-box__time-icon']}>
-                        <FontAwesomeIcon icon={faClock}/>
+        <div className={styles['summary-wrapper']}>
+            <div className={styles['summary']}>
+                <h1 className={styles['summary__title']}>{props.title}</h1>
+                <p dangerouslySetInnerHTML={{ __html: `${props.summary}` }}></p>
+                <aside className={styles['summary__time-info']}>
+                    <span className={styles['summary__time-icon']}>
+                        <FontAwesomeIcon icon={faClock} />
                     </span>
-                    <span className={styles['description-box__time-qty']}>{props.duration}m</span>
-                    <span className={styles['description-box__time-level']}>Beginner</span>
+                    <span className={styles['summary__time-qty']}>{props.duration}m</span>
+                    <span className={styles['summary__time-level']}>Beginner</span>
                 </aside>
             </div>
         </div>
     )
 }
 
-const Ingredients = () => {
+const IngredientsList = (props: { extendedIngredients: ExtendedIngredient[] }) => {
     return (
-        <div className={styles['ingredients']}>
-            <h3>Ingredients</h3>
-            <Text/>
-        </div>
+        <ul>
+            {
+                props.extendedIngredients.map((el, i) => (
+                    <li key={el.name} className={styles['ingredient']}>
+                        <span>{i + 1}</span>{el.original}
+                    </li>
+                ))
+            }
+        </ul>
+
     )
 }
 
-const RecipeSection = () => {
+const Instructions = (props: { steps: Step[] }) => {
     return (
         <div className={styles['instructions']}>
             <h3 className={styles['instructions-title']}>Instructions</h3>
             <ul className={styles['instructions-list']}>
-                <li><span>1</span><Text/></li>
-                <li><span>2</span><Text/></li>
-                <li><span>3</span><Text/></li>
-                <li><span>4</span><Text/></li>
+                {
+                    props.steps.map(el => (
+                        <li key={el.number}><span>{el.number}</span>{el.step}</li>
+                    ))
+                }
             </ul>
         </div>
     )
@@ -87,7 +107,7 @@ const Tip = () => {
     return (
         <div className={styles.tip}>
             <h3>Tip!</h3>
-            <Text/>
+            <Text />
         </div>
     )
 }
