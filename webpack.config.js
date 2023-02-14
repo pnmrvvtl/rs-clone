57da30ea968848bf1e48a42daa7923ceed8feef2
Zhanna Chaikovska <zhanna@pop-os.localdomain>
2023-02-14 13:39:33 +0200

    feat: add environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const EslingPlugin = require('eslint-webpack-plugin');
+const EslingPlugin = require("eslint-webpack-plugin");
 
-
-const mode = process.env.NODE_ENV || 'development';
-const devMode = mode === 'development';
-const target = devMode ? 'web' : 'browserslist';
-const devtool = devMode ? 'source-map' : undefined;
+const mode = process.env.NODE_ENV || "development";
+const devMode = mode === "development";
+const target = devMode ? "web" : "browserslist";
+const devtool = devMode ? "source-map" : undefined;
 
 module.exports = {
   mode,
@@ -17,78 +17,80 @@ module.exports = {
     port: 3000,
     open: true,
     hot: true,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-  entry: '/src/index.tsx',
+  entry: "/src/index.tsx",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     clean: true,
-    filename: '[name].[contenthash].js',
-    assetModuleFilename: 'assets/[hash][ext]',
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "assets/[hash][ext]",
     publicPath: "/",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html')
+      template: path.resolve(__dirname, "src", "index.html"),
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
+      filename: "[name].[contenthash].css",
     }),
     new EslingPlugin({
-      extensions: 'ts'
-    })
+      extensions: "ts",
+    }),
+    new Dotenv({
+      systemvars: false,}),
   ],
   module: {
     rules: [
       {
         test: /\.html$/i,
-        loader: 'html-loader',
+        loader: "html-loader",
       },
       {
         test: /\.(c|sa|sc)ss$/i,
         use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [require('postcss-preset-env')],
-              }
-            }
+                plugins: [require("postcss-preset-env")],
+              },
+            },
           },
-          'sass-loader'
+          "sass-loader",
         ],
       },
       {
         test: /\.woff2?$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'fonts/[name][ext]'
-        }
+          filename: "fonts/[name][ext]",
+        },
       },
       {
         test: /\.(jpe?g|png|webp|gif|svg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.m?js$/i,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
 };
