083a785b4d6256b280d4cb0331b8351caec20989
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-02-14 20:00:21 +0200

    fix: repair on close popup

--- src/pages/meals-plan-page/meals-plan-page.tsx
+++ src/pages/meals-plan-page/meals-plan-page.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import ReactPaginate from 'react-paginate';
