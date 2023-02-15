1f2a8e0dbe0c070508e2966b7952b375bebba599
Vladislav <104913853+Adwasar@users.noreply.github.com>
2023-02-15 18:59:33 +0200

    feat: refactor meal-page (#24)
    * feat: images for ingredients
    
    * style: fix adaptive
    
    * refactor: removed any types
    
    * refactor: set semantic tags
    
    * refactor: remove unused variables
    
    ---------
    
    Co-authored-by: Adwasar <user@Users-MacBook-Pro.local>

--- src/components/meal/meal.tsx
+++ src/components/meal/meal.tsx
@@ -1,13 +1,13 @@
+import { useParams } from "react-router";
+import { useContext, useEffect } from "react";
+
 import styles from './meal.module.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-solid-svg-icons';
-import { useParams } from "react-router";
-import { useContext, useEffect } from "react";
 import { UserContext } from "../../context/user-context";
 import ErrorPage from "../../pages/error-page/error-page";
-// import { CommentBankSharp } from '@mui/icons-material';
-import { ExtendedIngredient, Step } from "../../types/meals-api-types";
+import { ResultMeal, ExtendedIngredient, Step, Nutrient } from "../../types/meals-api-types";
 
 export default function Meal() {
     const { id } = useParams();
@@ -20,96 +20,42 @@ export default function Meal() {
     const meal = mealsByParametersResponse.results.filter((el) => el.id === +id!)[0];
 
     const { nutrients } = meal.nutrition;
-    console.log(nutrients);
     if (!meal) {
         return <ErrorPage />
     }
 
     return (
-        <div className={styles.container}>
+        <main className={styles.container}>
             <div className={styles['description-wrapper']}>
                 <div className={styles['description__content-wrapper']}>
                     <DescriptionBox title={meal.title} duration={meal.readyInMinutes} summary={meal.summary} />
                 </div>
                 <img
                     className={styles['description__meal-img']}
-                    src={`${meal.image}`}>
+                    src={`${meal.image}`}
+                    alt="dish">
                 </img>
             </div>
 
             <div className={styles['content-wrapper']}>
                 <div className={styles.content}>
-                    <div className={styles['ingredients']}>
+                    <section className={styles['ingredients']}>
                         <h3>Ingredients</h3>
-                        <IngredientsList extendedIngredients={meal.extendedIngredients} />
-                    </div>
-
-
+                        <IngredientsList
+                            extendedIngredients={meal.extendedIngredients}
+                        />
+                    </section>
 
                     <div className={styles['main-content']}>
                         <div className={styles['composition__other']}>
                             <Instructions steps={meal.analyzedInstructions[0].steps} />
-
-                            <div className={styles['composition__nutrients']}>
-                                <h3 className={styles['composition__title']}>
-                                    Nutritional Information
-                                </h3>
-                                <div className={styles['composition__blocks']}>
-                                    <div className={styles['composition__block-carbs']}>
-                                        <div className={styles['composition__block-line']}></div>
-                                        <span className={styles['composition__block-name']}>NET CARBS</span>
-                                        <span className={styles['composition__block-weight']}>
-                                            {nutrients.filter((el: any) => el.name === "Net Carbohydrates")[0].amount}g
-                                        </span>
-                                        <span className={styles['composition__block-percent']}>
-                                            {meal.nutrition.caloricBreakdown.percentCarbs}%
-                                        </span>
-                                    </div>
-                                    <div className={styles['composition__block-protein']}>
-                                        <div className={styles['composition__block-line']}></div>
-                                        <span className={styles['composition__block-name']}>PROTEIN</span>
-                                        <span className={styles['composition__block-weight']}>
-                                            {nutrients.filter((el: any) => el.name === "Protein")[0].amount}g
-                                        </span>
-                                        <span className={styles['composition__block-percent']}>
-                                            {meal.nutrition.caloricBreakdown.percentProtein}%
-                                        </span>
-                                    </div>
-                                    <div className={styles['composition__block-fat']}>
-                                        <div className={styles['composition__block-line']}></div>
-                                        <span className={styles['composition__block-name']}>FAT</span>
-                                        <span className={styles['composition__block-weight']}>
-                                            {nutrients.filter((el: any) => el.name === "Fat")[0].amount}g
-                                        </span>
-                                        <span className={styles['composition__block-percent']}>
-                                            {meal.nutrition.caloricBreakdown.percentFat}%
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles['composition__line']}>
-                                    {
-                                        nutrients.map((el: any) =>
-                                            <div className={styles['composition__item']}>
-                                                <span className={styles['name']}>{el.name}</span>
-                                                <div className={styles['property-persens']}>
-                                                    <span className={styles['property']}>{el.amount}{el.unit}</span>
-                                                    <span className={styles['persens']}>
-                                                        {el.percentOfDailyNeeds}%
-                                                    </span>
-                                                </div>
-
-                                            </div>
-                                        )
-                                    }
-                                </div>
-                            </div>
+                            <Nutritional nutrients={nutrients} meal={meal} />
                         </div>
                     </div>
                 </div>
             </div>
             <div className={styles['for-margin-bottom']}></div>
-        </div>
+        </main>
     )
 }
 
@@ -117,7 +63,7 @@ export default function Meal() {
 const DescriptionBox = (props: { title: string, duration: number, summary: string }) => {
     return (
         <div className={styles['summary-wrapper']}>
-            <div className={styles['summary']}>
+            <section className={styles['summary']}>
                 <h1 className={styles['summary__title']}>{props.title}</h1>
                 <p dangerouslySetInnerHTML={{ __html: `${props.summary}` }}></p>
                 <aside className={styles['summary__time-info']}>
@@ -127,29 +73,35 @@ const DescriptionBox = (props: { title: string, duration: number, summary: strin
                     <span className={styles['summary__time-qty']}>{props.duration}m</span>
                     <span className={styles['summary__skill-level']}>Hard</span>
                 </aside>
-            </div>
+            </section>
         </div>
     )
 }
 
 const IngredientsList = (props: { extendedIngredients: ExtendedIngredient[] }) => {
     return (
-        <ul>
+        <ul className={styles['ingredients-list']}>
             {
-                props.extendedIngredients.map((el, i) => (
+                props.extendedIngredients.map((el) => (
                     <li key={el.name} className={styles['ingredient']}>
-                        <span>{i + 1}</span>{el.original}
+                        <span>{el.amount} {el.unit}</span>
+                        <div className="image-wrapper">
+                            <img
+                                src={"https://spoonacular.com/cdn/ingredients_100x100/" + el.image}
+                                alt="ingredients"
+                            />
+                        </div>
+                        <span className={styles.name}>{el.name}</span>
                     </li>
                 ))
             }
         </ul>
-
     )
 }
 
 const Instructions = (props: { steps: Step[] }) => {
     return (
-        <div className={styles['instructions']}>
+        <section className={styles['instructions']}>
             <h3 className={styles['instructions-title']}>Instructions</h3>
             <ul className={styles['instructions-list']}>
                 {
@@ -158,6 +110,64 @@ const Instructions = (props: { steps: Step[] }) => {
                     ))
                 }
             </ul>
-        </div>
+        </section>
+    )
+}
+
+const Nutritional = (props: { nutrients: Nutrient[], meal: ResultMeal }) => {
+    return (
+        <section className={styles['composition__nutrients']}>
+            <h3 className={styles['composition__title']}>
+                Nutritional Information
+            </h3>
+            <div className={styles['composition__blocks']}>
+                <div className={styles['composition__block-carbs']}>
+                    <div className={styles['composition__block-line']}></div>
+                    <span className={styles['composition__block-name']}>NET CARBS</span>
+                    <span className={styles['composition__block-weight']}>
+                        {props.nutrients.filter((el) => el.name === "Net Carbohydrates")[0].amount}g
+                    </span>
+                    <span className={styles['composition__block-percent']}>
+                        {props.meal.nutrition.caloricBreakdown.percentCarbs}%
+                    </span>
+                </div>
+                <div className={styles['composition__block-protein']}>
+                    <div className={styles['composition__block-line']}></div>
+                    <span className={styles['composition__block-name']}>PROTEIN</span>
+                    <span className={styles['composition__block-weight']}>
+                        {props.nutrients.filter((el) => el.name === "Protein")[0].amount}g
+                    </span>
+                    <span className={styles['composition__block-percent']}>
+                        {props.meal.nutrition.caloricBreakdown.percentProtein}%
+                    </span>
+                </div>
+                <div className={styles['composition__block-fat']}>
+                    <div className={styles['composition__block-line']}></div>
+                    <span className={styles['composition__block-name']}>FAT</span>
+                    <span className={styles['composition__block-weight']}>
+                        {props.nutrients.filter((el) => el.name === "Fat")[0].amount}g
+                    </span>
+                    <span className={styles['composition__block-percent']}>
+                        {props.meal.nutrition.caloricBreakdown.percentFat}%
+                    </span>
+                </div>
+            </div>
+
+            <div className={styles['composition__line']}>
+                {
+                    props.nutrients.map((el) =>
+                        <div className={styles['composition__item']}>
+                            <span className={styles['name']}>{el.name}</span>
+                            <div className={styles['property-persens']}>
+                                <span className={styles['property']}>{el.amount}{el.unit}</span>
+                                <span className={styles['persens']}>
+                                    {el.percentOfDailyNeeds}%
+                                </span>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+        </section>
     )
 }
